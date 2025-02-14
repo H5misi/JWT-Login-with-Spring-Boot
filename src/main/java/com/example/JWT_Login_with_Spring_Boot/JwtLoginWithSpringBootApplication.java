bf8030212a9e51dff7dc42b4d9cf0b4b6b8fb47d
H5misi <hazm.sayed.m@gmail.com>
2025-02-14 02:53:31 +0200

    Spring-initializr files

---
+++ src/main/java/com/example/JWT_Login_with_Spring_Boot/JwtLoginWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_Login_with_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtLoginWithSpringBootApplication.class, args);
+	}
+
+}
