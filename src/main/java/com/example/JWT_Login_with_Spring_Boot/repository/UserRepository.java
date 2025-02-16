9cb98bcbde05b02e78dba0ce2067ff30614bc29c
H5misi <hazm.sayed.m@gmail.com>
2025-02-16 03:40:13 +0200

    Refactor UserRepository to extend CrudRepository for basic CRUD operations

--- src/main/java/com/example/JWT_Login_with_Spring_Boot/repository/UserRepository.java
+++ src/main/java/com/example/JWT_Login_with_Spring_Boot/repository/UserRepository.java
@@ -2,20 +2,27 @@ package com.example.JWT_Login_with_Spring_Boot.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.JWT_Login_with_Spring_Boot.model.User;
 
 
 /**
- * Repository interface for handling User entity database operations.
+ * Repository interface for handling User entity database operations for User entities.
  * @Repository marks this as a Spring Data Access Object (DAO) component and enables:
  * - Automatic exception translation
  * - Component scanning
  * - Spring bean registration
  */
 @Repository
-public interface UserRepository {
+/**
+ * This declaration that provides basic CRUD (Create, Read, Update, Delete) operations for the User entity.
+ * CrudRepository<User, Long>:
+ * - User: The entity type this repository will manage
+ * - Long: The type of the primary key (ID) for the User entity
+*/
+public interface UserRepository extends CrudRepository<User, Long> {
     /**
      * Optional<T>: is a safe way to handle potentially null results. If no user is found with the
      * given email(or anything else), it will return an empty Optional rather than null.
