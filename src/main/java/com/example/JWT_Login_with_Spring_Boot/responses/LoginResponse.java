1846f203c909717d8bd599028e02b50be6856955
H5misi <hazm.sayed.m@gmail.com>
2025-03-17 17:54:28 +0200

    Add LoginResponse to encapsulate response data for successful login, including token and expiration details

---
+++ src/main/java/com/example/JWT_Login_with_Spring_Boot/responses/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.example.JWT_Login_with_Spring_Boot.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * simple container to represent the data sent back (response) to a client after a successful login
+ */
+@Getter
+@Setter
+public class LoginResponse {
+    
+    private String token;
+    private String expiresIn; // indicates the duration until the token expires, typically in seconds or milliseconds
+
+
+    public LoginResponse(String token, String expiresIn) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+    }
+
+    
+}
